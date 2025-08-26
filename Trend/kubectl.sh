c8882de5c7de58352ec95d834e607c08920033e6
root <root@ip-172-31-32-73.ap-south-1.compute.internal>
2025-08-26 13:36:40 +0000

    fix Trend folder as normal code

---
+++ Trend/kubectl.sh
@@ -0,0 +1,19 @@
+curl -LO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
+tar -xzf eksctl_Linux_amd64.tar.gz
+sudo mv eksctl /usr/local/bin
+
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+
+chmod +x ./aws-iam-authenticator
+
+mv ./aws-iam-authenticator /usr/local/bin
+
+aws-iam-authenticator version
+
+kubectl version --client
+
+eksctl version
